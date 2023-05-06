0fe8178637baa5471d7c27431578c63b580977dd
Om Gupta <86357151+AumGupta@users.noreply.github.com>
2023-05-06 18:14:32 +0530

    Update semi_bold_text.dart

--- lib/widgets/semi_bold_text.dart
+++ lib/widgets/semi_bold_text.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
 class SemiBoldText extends StatelessWidget {
   String text;
   double size;
@@ -11,6 +12,7 @@ class SemiBoldText extends StatelessWidget {
   Widget build(BuildContext context) {
     return Text(
       text,
+      overflow: TextOverflow.ellipsis,
       style: GoogleFonts.poppins(
         textStyle: TextStyle(
           fontWeight: FontWeight.w600,
